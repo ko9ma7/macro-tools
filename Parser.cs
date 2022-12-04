60ae8b0041ea2d99d898302779b362e8bb26eb9e
Luka Kourtev <lukakourtev@peters-mbp.lan>
2022-12-03 21:19:52 -0500

    small changes

--- Parser.cs
+++ Parser.cs
@@ -4,7 +4,23 @@ namespace Macroc
     {
         public enum Opcode
         {
-            Add = (byte)0x00,
+            Ldvar = 0x00,
+            Ldfloat,
+            Ldint,
+            Ldstring,
+            Stovar,
+            Add = 0x10,
+            Sub,
+            Mult,
+            Div,
+        }
+
+        public enum Register
+        {
+            R1,
+            R2,
+            R3,
+            R4
         }
 
         private enum VarType
@@ -16,11 +32,17 @@ namespace Macroc
         private List<Token> Toks;
         private Token Current;
         private int CurPos;
+        private bool IsValid;
+        private Dictionary<string, VarType> VarTable;
+        private Dictionary<string, int> FuncTable;
         public Parser(List<Token> toks)
         {
             Toks = toks;
             Current = Toks[0];
             CurPos = 0;
+            IsValid = true;
+            VarTable = new();
+            FuncTable = new();
         }
 
         private TokenType PeekType()
@@ -28,7 +50,7 @@ namespace Macroc
             return Toks[CurPos + 1].Type;
         }
         
-        private void Next(bool allowEOF)
+        private void Next(bool allowEOF = false)
         {
             CurPos++;
             if (CurPos >= Toks.Count)
@@ -44,6 +66,28 @@ namespace Macroc
             Current = Toks[CurPos];
         }
 
+        private List<byte> ParseOperandLoad(Token operand, Register reg)
+        {
+            List<byte> bytes = new();
+
+            switch (operand.Type)
+            {
+                case TokenType.Ident:
+                string Ident = ((IdentToken)operand).Ident;
+                break;
+                case TokenType.Int:
+                break;
+                case TokenType.Float:
+                break;
+                default:
+                Console.WriteLine($"Error: expected number or ident (Line {operand.Line + 1}");
+                IsValid = false;
+                break;
+            }
+
+            return bytes;
+        }
+
         public List<byte> Parse()
         {
             List<byte> bytes = new();
@@ -55,11 +99,28 @@ namespace Macroc
                 switch (Current.Type)
                 {
                     case TokenType.EOS:
+                        if (!IsValid) Environment.Exit(-4);
                         return bytes;
                     case TokenType.Ident:
                         if (PeekType() == TokenType.Operator)
                         {
-                            // Expression
+                            string assignee = ((IdentToken)Current).Ident;
+                            Next();
+
+                            if (((OperatorToken)Current).Operator != OperatorType.Assign)
+                            {
+                                Console.WriteLine($"Error: Expected assignment operator (Line {Current.Line + 1}) ");
+                                IsValid = false;
+                            }
+
+                            Next();
+                            Token left = Current;
+                            Next();
+                            Token op = Current;
+                            Next();
+                            Token right = Current;
+
+
                         }
                         else
                         {
